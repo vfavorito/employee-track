a434fa771d69c4f295cd4e72ba736e6a64c11c33
vfavorito <vince.favorito@gmail.com>
2021-01-02 11:40:33 -0600

    Wrote start function to direct what you want to do

--- app.js
+++ app.js
@@ -11,4 +11,37 @@ const connection = mysql.createConnection({
 
 connection.connect(function (error) {
   if (error) throw error;
+  start();
 });
+
+function start() {
+    inquirer.prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["Add a Department", "Add a Role", "Add an Employee", "View All Departments", "View All Roles", "View All Employees", "Update Employee Role"]
+    }).then(function (response) {
+      switch (response.action) {
+        case "Add a Department":
+          addDepartment();
+          break;
+        case "Add a Role":
+          addRole();
+          break;
+        case "Add an Employee":
+          addEmployee();
+          break;
+        case "View All Departments":
+          viewDepartment();
+          break;
+        case "View All Roles":
+          viewRole();
+          break;
+        case "View All Employees":
+          viewEmployee();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+      }
+    });
+  };
